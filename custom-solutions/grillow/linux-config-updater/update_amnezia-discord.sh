3b773e59598d8ce083c1a4565551237fd682a3d0
grillow <spmr757@gmail.com>
2024-10-18 21:59:33 +0300

    fix

--- custom-solutions/grillow/linux-config-updater/update_amnezia-discord.sh
+++ custom-solutions/grillow/linux-config-updater/update_amnezia-discord.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-discord_ips="$(wget -O- https://github.com/GhostRooter0953/discord-voice-ips/raw/refs/heads/master/custom-solutions/KindWarlock/discord-main-ips 2>/dev/null | sed -s 's/\r//g' | sort -u | tr '\n' ',' | sed 's/,$//')"
+discord_ips="$(wget -O- https://github.com/GhostRooter0953/discord-voice-ips/raw/refs/heads/master/amnezia/amnezia-discord-domains.json 2>/dev/null | jq -r 'map(select(.ip != "")) | map(.ip) | join(",")')"
 sudo sed -i "/# Discord/{n;s|^AllowedIPs = .*|AllowedIPs = $discord_ips|}" "$CONFIG_PATH"
 
-discord_voice_ips="$(wget -O- https://github.com/GhostRooter0953/discord-voice-ips/raw/refs/heads/master/amnezia-voice-ip.json 2>/dev/null | jq -r 'map(.ip) | join(",")')"
+discord_voice_ips="$(wget -O- https://github.com/GhostRooter0953/discord-voice-ips/raw/refs/heads/master/amnezia/amnezia-voice-ip.json 2>/dev/null | jq -r 'map(select(.ip != "")) | map(.ip) | join(",")')"
 sudo sed -i "/# Discord\ Voice/{n;s|^AllowedIPs = .*|AllowedIPs = $discord_voice_ips|}" "$CONFIG_PATH"
